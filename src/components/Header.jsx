65297ec9bdbf59527e0d69a8c17cf09740dab448
Remi Tondeux <remi.tondeux@gmail.com>
2025-06-24 21:45:48 +0200

    ambiance music management 100% + min-width on all buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,13 @@ import { Button } from '../components/Button'
 import { ResourcesPanel } from '../components/ResourcesPanel'
 
 export function Header({team, gold, onButtonOne, onButtonTwo, buttonOne, buttonTwo, muted, onMute}) {
+
+  const musicButtonValue = <img
+    onClick={onMute}
+    src={muted ? `${import.meta.env.BASE_URL}/images/sound-off.png` : `${import.meta.env.BASE_URL}/images/sound-on.png`}
+    alt={muted ? "Sound off" : "Sound on"} />
+
+
   return <div className="header wrapper d-flex align-items-center justify-content-between">
     <div className="d-flex">
       {team.map(member => (
@@ -14,7 +21,7 @@ export function Header({team, gold, onButtonOne, onButtonTwo, buttonOne, buttonT
     </div>
     <ResourcesPanel gold={gold} />
     <div>
-      <Button value={muted ? "On" : "Off"} onClick={onMute}/>
+      <Button value={musicButtonValue} onClick={onMute} />
       <Button value={buttonOne} onClick={onButtonOne} />
       <Button value={buttonTwo} onClick={onButtonTwo} />
     </div>
